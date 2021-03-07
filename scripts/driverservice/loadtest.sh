2af604e2b80be14d53ff1df5c94cd3104836105a
Nicolai Gram <nicolaigram@gmail.com>
2021-03-07 13:47:17 +0100

    Driverservice loadtest script

--- scripts/driverservice/loadtest.sh
+++ scripts/driverservice/loadtest.sh
@@ -1,12 +1,21 @@
+# Check if .env is in directory
+if ! test -f ".env"; then
+    echo "No .env found. You need to manually add it."
+    exit 2
+fi
+
 # DriverService
-ssh admin@10.123.252.250 rm -rf DriverService # Folder might already exist
-ssh admin@10.123.252.250 git clone https://github.com/RealTimeStreaming-BachelorProject/DriverService.git
-ssh admin@10.123.252.250 sudo docker-compose -f ./DriverService/docker-compose.yml build
-ssh admin@10.123.252.250 sudo docker-compose -f ./DriverService/docker-compose.yml up -d
+server_driver="admin@10.123.252.250"
+ssh $server_driver rm -rf DriverService # Folder might already exist
+ssh $server_driver git clone https://github.com/RealTimeStreaming-BachelorProject/DriverService.git
+scp .env $server_driver:~/DriverService
+ssh $server_driver sudo docker-compose -f ./DriverService/docker-compose.yml build
+ssh $server_driver sudo docker-compose -f ./DriverService/docker-compose.yml up -d
 
 # Artillery.io
-ssh admin@10.123.252.251 rm -rf ClientTestingTool # Folder might already exist
-ssh admin@10.123.252.251 git clone https://github.com/RealTimeStreaming-BachelorProject/ClientTestingTool.git
-ssh admin@10.123.252.251 sudo docker-compose -f ./ClientTestingTool/loadtest/docker-compose.yml build
-ssh admin@10.123.252.251 sudo docker-compose -f ./ClientTestingTool/loadtest/docker-compose.yml run test_runner "./tests/driverService/driverservice.loadtest.yml" "--target http://10.123.252.250:5002"
+server_test="admin@10.123.252.251"
+ssh $server_test rm -rf ClientTestingTool # Folder might already exist
+ssh $server_test git clone https://github.com/RealTimeStreaming-BachelorProject/ClientTestingTool.git
+ssh $server_test sudo docker-compose -f ./ClientTestingTool/loadtest/docker-compose.yml build
+ssh $server_test sudo docker-compose -f ./ClientTestingTool/loadtest/docker-compose.yml run test_runner "./tests/driverService/driverservice.loadtest.yml" "--target http://10.123.252.250:5002"
 # TODO: ^Above command doesn't execute --target argument.
